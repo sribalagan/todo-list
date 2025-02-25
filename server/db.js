4711a5b4849fe6438f932f7d9347cb99cf962b38
sri balagan <77321697+sribalagan@users.noreply.github.com>
2025-02-25 10:44:11 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool
 
 const pool = new Pool({
     user : "postgres",
-    password :"seemasri",
+    password :"",
     host : "localhost",
     port : "5432",
     database : "todo"
