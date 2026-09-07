b4248cff8727d2a8e302866c18231c9cd42a69fd
sribalagan <sribalagan.21@gmail.com>
2026-09-07 23:11:02 +0530

    updated env variables

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user : "postgres",
-    password :"",
-    host : "localhost",
-    port : "5432",
-    database : "todo"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
